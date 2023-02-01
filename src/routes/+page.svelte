4b63fd0330ba9b2ad0b80556ca1858a00c5ec82d
nown1ne <abhinav14863@gmail.com>
2023-02-01 10:47:56 +0530

    done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1 class="text-4xl p-4 bg-blue-400 text-center font-bold underline">
+    Hello world!
+</h1>
+
+
+<style lang="postcss">
+    :global(html) {
+      background-color: white;
+    }
+    h1{
+        margin-top: 2em;
+    }
+  </style>
